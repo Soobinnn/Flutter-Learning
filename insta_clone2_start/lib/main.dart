70994f9c3f585a3134d0945ba7b522e40ffa4d6b
임수빈 <sbim@cafe24corp.com>
2020-10-23 18:37:36 +0900

    add project

---
+++ insta_clone2_start/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'root_page.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    print('MyApp created');
+
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: Colors.white,
+        accentColor: Colors.black,
+      ),
+      home: RootPage(),
+    );
+  }
+}
