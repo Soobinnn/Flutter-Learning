5abb2b8516c168788b155cb933c0f45bd8704b2d
임수빈 <sbim@cafe24corp.com>
2020-11-19 19:00:30 +0900

    example

---
+++ image_picker_app/android/app/src/main/kotlin/com/bsidep/image_picker_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.bsidep.image_picker_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ note_app/android/app/src/main/kotlin/com/bsidep/note_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.bsidep.note_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
